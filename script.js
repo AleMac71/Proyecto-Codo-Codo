bca0695f16688b76eaa592629df4ccf8edfddae1
manuale1871 <manuale1871@gmail.com>
2024-06-07 21:39:50 +0000

    Api.Fech2

--- script.js
+++ script.js
@@ -3,20 +3,17 @@ const botonAnterior = document.getElementsByClassName("boton_anterior");
 const botonSiguiente = document.getElementsByClassName("boton_siguiente");
 
 let pagina = 1;
-botonAnterior.addEventListener("click", () => {
-  if (pagina > 1) {
+function antes (){
+  if (pagina >1) {
     pagina -= 1;
+    traerPeliculas();}}
+  
+function despues (){
+  if (pagina <=1000){
+    pagina +=1;
     traerPeliculas();
   }
-});
-
-
-botonSiguiente.addEventListener("click", () => {
-  if (pagina <= 1000) {
-    pagina += 1;
-    traerPeliculas();
-  }
-});
+}
 
 const traerPeliculas = async () => {
   try {
@@ -30,12 +27,9 @@ const traerPeliculas = async () => {
     let peliculas = [];
     datos.results.forEach(pelicula => {
       peliculas +=
-        `<div id="pelicula" class="card" style="width: 18rem;">
+        `<div id="pelicula" class="card" style="width: 20vw;height: 28vw">
          <img src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}" class="card-img-top" alt="peliculas">
-            <div>
-                 <h5 class="card-title titulo">${pelicula.title}</h5>
-                  <p class="card-text descripcion">${pelicula.overview}</p>
-            </div>
+            
         </div>`
     });
     grupPeliculas.innerHTML = peliculas;
